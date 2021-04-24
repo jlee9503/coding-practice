1c2ced4c13fbc078db53a86537fbe39a0e8d97fe
jlee9503 <jungsu9503@gmail.com>
2021-04-23 21:57:19 -0400

    Create DateAndTime.java

---
+++ Hackerrank/Java/DateAndTime.java
@@ -0,0 +1,23 @@
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.MONTH, month-1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.set(Calendar.YEAR, year);
+        
+        return calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();        
+    
+    }
+
+}
